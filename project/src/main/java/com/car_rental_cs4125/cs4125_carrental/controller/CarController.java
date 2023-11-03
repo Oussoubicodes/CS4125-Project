532d870bf5401217638bb87d8e36134cbb96af8a
Oussoubicodes <130454123+Oussoubicodes@users.noreply.github.com>
2023-11-03 23:37:45 +0000

    Add files via upload

---
+++ project/src/main/java/com/car_rental_cs4125/cs4125_carrental/controller/CarController.java
@@ -0,0 +1,33 @@
+package com.car_rental_cs4125.cs4125_carrental.controller;
+
+import org.springframework.stereotype.Controller;
+import com.car_rental_cs4125.cs4125_carrental.model.Car;
+import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import java.util.List;
+
+@Controller
+public class CarController {
+    private List<Car> availableCars;
+    //private List<CarReservation> reservation; 
+
+    public void addCar(Car car){
+        availableCars.add(car);
+    }
+
+    public void removeCar(Car car){
+        availableCars.remove(car);
+    }
+
+    /*public void editCar(Car car){
+        Car newCar;
+        this.car = newCar;
+    }*/
+
+   @GetMapping("/browse")
+    public ModelAndView viewCar(Car car){
+        ModelAndView modelAndView = new ModelAndView();
+    }
+
+}
